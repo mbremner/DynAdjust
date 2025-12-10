2639dbe4f24b06d01438a8a5f6bb96ff7cf4e88a
mbremner <m_b2@hotmail.ca>
2025-12-10 14:31:48 -0800

    Uncommented Reftran Exception, added std namespace to string type

--- dynadjust/include/functions/dnatransparamfuncs.hpp
+++ dynadjust/include/functions/dnatransparamfuncs.hpp
@@ -2448,7 +2448,7 @@ void determineNAD83Parameters(transformation_parameter_set& tParam)
 	case GDA94_i_2d:
 	case GDA94_i:
 		// No direct parameters exist!
-		throw RefTranException(message_parameters_undefined<string, UINT32>(tParam.from_to_.first, tParam.from_to_.second),
+		throw RefTranException(message_parameters_undefined<std::string, UINT32>(tParam.from_to_.first, tParam.from_to_.second),
 			REFTRAN_DIRECT_PARAMS_UNAVAILABLE);
 		break;
 	//////////////////////////////////
